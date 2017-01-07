59d1baa2adc7012d413a61e9b94781a0d52b33a5
tdm <you@example.com>
2017-01-07 12:13:08 -0500

    test waitcursor

--- testwaitcursor/mainwindow.cpp
+++ testwaitcursor/mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "WaitCursor.h"
+#include <QMessageBox>
+#include <QThread>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    Gui::WaitCursor wc;
+    QThread::sleep(5);
 }
