b7368a22680df3cf36ca91a8943a39240fc39fe5
tdm <you@example.com>
2017-01-08 19:16:09 -0500

    update run.sh

--- testsheettableview/run.sh
+++ testsheettableview/run.sh
@@ -1,1 +1,4 @@
-/usr/bin/python2.7 generate.py --outputPath src/App/ PropertyContainerPy.xml
+#/usr/bin/python2.7 generate.py --outputPath src/App/ PropertyContainerPy.xml
+$qmake -project QT+=widgets INCLUDEPATH+=/usr/include/python2.7/ INCLUDEPATH+=./Base INCLUDEPATH+=./Gui INCLUDEPATH+=./App INCLUDEPATH+=./Spreadsheet CONFIG+=c++14 QT+=xml QT+=printsupport QT+=uitools QT+=network QT+=opengl  QT+=svg && qmake && make 
+
+
